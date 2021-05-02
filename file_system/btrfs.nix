0c1704b3dd95ae98ac6287cec46e648f24d762f5
jkimmerling <80171344+jkimmerling@users.noreply.github.com>
2021-05-02 14:07:48 -0400

    Create btrfs.nix

---
+++ file_system/btrfs.nix
@@ -0,0 +1,29 @@
+
+{ config, lib, pkgs, ... }:
+
+let
+  espDev = "/dev/disk/by-uuid/123B-7989";
+  btrfsDev = "/dev/disk/by-uuid/84e95073-df93-45d8-a9b1-df5c43a4989c";
+  swapDev = "/dev/disk/by-uuid/e1a6928f-faa8-4eca-8de0-6d2778986ae1";
+
+  subvolume = name: {
+    device = btrfsDev;
+    fsType = "btrfs";
+    options = [ "subvol=/${name}" "compress-force=zstd" ];
+  };
+
+in {
+  fileSystems = {
+    "/" = subvolume "@";
+    "/home" = subvolume "@home";
+    "/nix" = subvolume "@nix";
+    "/.subvols" = subvolume "";
+
+    "/boot" = {
+      device = espDev;
+      fsType = "vfat";
+    };
+  };
+
+  swapDevices = [ { device = swapDev; } ];
+}
